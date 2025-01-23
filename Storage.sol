25aa1afd83dd03ba84e377ff36343e7c85ad4c56
Eros254 <Eros254>
2025-01-23 19:06:01 +0300

    add code

--- Storage.sol
+++ Storage.sol
@@ -3,7 +3,17 @@
 pragma solidity >0.6.0 <= 0.9.0;
 
 contract Storage{
+    //This will be initialized to zero
     uint256 newNumber;
+    bool newBool;
+
+    struct People{
+      uint256 newNumber;
+      string name;
+    }
+
+    People[] public  people;
+    mapping(string => uint256) public nameToNewNumber;
 
 
     function store(uint256 _newNumber) public{
